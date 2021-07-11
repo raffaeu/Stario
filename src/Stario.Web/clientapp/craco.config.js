5ae5244158315742b9a0a65a7ea1ad0278cca416
Raffaeu <raffaeu@gmail.com>
2021-07-11 18:34:43 +0200

    Cracco with Dark theme

--- src/Stario.Web/clientapp/craco.config.js
+++ src/Stario.Web/clientapp/craco.config.js
@@ -1,4 +1,5 @@
 ﻿const CracoLessPlugin = require('craco-less');
+const { getThemeVariables } = require('antd/dist/theme');
 
 module.exports = {
     plugins: [
@@ -7,7 +8,9 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#1DA57A' },
+                        modifyVars: getThemeVariables({
+                            dark: true
+                        }),
                         javascriptEnabled: true,
                     },
                 },
